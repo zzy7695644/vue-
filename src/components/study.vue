071ca352a3721e95249817665220429d7ee4ecf2
zzy7695644 <7695644@qq.com>
2020-07-28 16:33:16 +0800

    style样式

--- src/components/study.vue
+++ src/components/study.vue
@@ -1,19 +1,25 @@
 <template>
-  <!-- <div v-bind:class="[activeClass, errorClass]"></div> -->
-  <!-- <div v-bind:class="[isActive ? activeClass : '', errorClass]"></div> -->
-  <!-- isActive不为truthy真值（除 false、0、""、null、undefined 和 NaN 以外皆为真值） -->
-  <div v-bind:class="[{ active: activeClass }, errorClass]"></div>
-  <!-- 对象里的值是由data里数据的真假值决定 -->
-  <!-- 疑问：class属性只取key值 -->
+  <!-- <div v-bind:style="{ color: activeColor, fontSize: fontSize+'px'}"></div> -->
+  <!-- <div v-bind:style="styleObject">1111111111111111111111111111111111</div> -->
+  <!-- 直接绑定样式对象，return里应该是样式名加参数 -->
+  <!-- <div v-bind:style="[baseStyles, overridingStyles]">111111111111111111111111</div> -->
+  <!-- 绑定多个样式对象 -->
+  <div :style="{ display: ['-webkit-box', '-ms-flexbox', 'flex'] }">22222222222222222</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-       activeClass: true,
-       errorClass: 'text-danger'
+      baseStyles: {
+        color: 'red'
+      },
+      overridingStyles:{
+        fontSize: 30
+      }
+      
     }
-  }
+  },
+  
 }
 </script>
