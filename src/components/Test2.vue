6f0b4d6673cb3fa6efe026bfa0eb4b4729afbf90
zzy7695644 <7695644@qq.com>
2020-07-29 16:26:39 +0800

    多选框

--- src/components/Test2.vue
+++ src/components/Test2.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    <input type="checkbox" id="checkbox" v-model="checked">
-    <label for="checkbox">{{checked}}</label>
+    <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+    <label for="jack">Jack</label>
+    <input type="checkbox" id="john" value="John" v-model="checkedNames">
+    <label for="john">John</label>
+    <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+    <label for="mike">Mike</label>
+    <br>
+    <span>Checked names: {{ checkedNames }}</span>
+    <!-- 多个复选框绑定到同一个数组 -->
   </div>
 </template>
 
@@ -9,7 +16,7 @@
 export default {
   data() {
     return{
-      checked: 'false'
+      checkedNames: ['ssss']
     }
   }
 }
